cd3e8981d8128fa72a3a68c26e0595af613c5b47
Mundanity Fan <i@mundanity.moe>
2022-07-02 10:15:53 +0800

    新增开黑啦 api 类

---
+++ KaiheilaBot/httpAPI/SendMessage.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Kaiheila\httpAPI;
+
+use Swlib\Saber;
+
+class SendMessage
+{
+    public $httpConn;
+
+    public function __construct($token)
+    {
+        $this->httpConn = Saber::create([
+            'base_uri' => "https://www.kaiheila.cn",
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bot ' . $token
+            ]
+        ]);
+    }
+
+    public function sendText($content, $target_id, $is_quote = false, $quote = '', $type = 1)
+    {
+        if ($is_quote) {
+            $reply = array(
+                'type' => $type,
+                'target_id' => $target_id,
+                'content' => $content,
+                'quote' => $quote
+            );
+        } else {
+            $reply = array(
+                'type' => $type,
+                'target_id' => $target_id,
+                'content' => $content
+            );
+        }
+        $this->httpConn->post('/api/v3/message/create', json_encode($reply));
+    }
+}
