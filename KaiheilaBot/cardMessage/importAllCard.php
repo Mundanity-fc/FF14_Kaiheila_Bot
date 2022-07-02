2cca0364fc2766c2b2a8ae01e1a4c6d3a3fd052d
Mundanity Fan <i@mundanity.moe>
2022-07-02 11:50:44 +0800

    新增卡片导入工具

---
+++ KaiheilaBot/cardMessage/importAllCard.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . '/Card.php';
+require __DIR__ . '/Divider.php';
+require __DIR__ . '/Image.php';
+require __DIR__ . '/ImageText.php';
+require __DIR__ . '/MultiColumnText.php';
+require __DIR__ . '/PlainText.php';
