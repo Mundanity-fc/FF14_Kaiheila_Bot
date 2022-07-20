acce527e429403bd1fe02ba10777f2e01f641d22
Mundanity Fan <i@mundanity.moe>
2022-07-20 12:28:58 +0800

    新增服务创建脚本

---
+++ installService.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+path="`pwd`/main.php"
+stdlog="`pwd`/journal.log"
+errlog="`pwd`/errors.log"
+service="/lib/systemd/system/FF14Bot.service"
+if test -e $service
+then
+  echo "文件已存在，请执行 uninstallSercive.sh 脚本将文件删除后再进行操作"
+  exit
+fi
+echo "[Unit]" >> $service
+echo "Description=FF14Bot Service" >> $service
+echo "Documentation=https://github.com/Mundanity-fc/FF14_Kaiheila_Bot" >> $service
+echo "" >> $service
+echo "[Service]" >> $service
+echo "Type=simple" >> $service
+echo "User=root" >> $service
+echo "LimitNPROC=500" >> $service
+echo "LimitNOFILE=1000000" >> $service
+echo "ExecStart=/usr/bin/php $path" >> $service
+echo "Restart=on-failure" >> $service
+echo "StandardOutput=append:$stdlog" >> $service
+echo "StandardError=append:$errlog" >> $service
+echo "" >> $service
+echo "[Install]" >> $service
+echo "WantedBy=multi-user.target" >> $service
+if test -e $service
+then
+  echo "已创建服务，请运行 systemctl start FF14Bot 执行该服务"
+  exit
+else
+  echo "服务创建失败，请确认脚本是否拥有权限（以 root 用户执行或采用 sudo 命令）"
+  exit
+fi
---
+++ uninstallService.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+service="/lib/systemd/system/FF14Bot.service"
+if test -e $service
+then
+  echo "正在删除服务……"
+else
+  echo "未找到服务，请确认是否正常安装"
+  exit
+fi
+rm -f $service
+if test -e $service
+then
+  echo "删除失败，请确认脚本是否拥有权限（以 root 用户执行或采用 sudo 命令）"
+  exit
+else
+  echo "删除成功"
+  exit
+fi
