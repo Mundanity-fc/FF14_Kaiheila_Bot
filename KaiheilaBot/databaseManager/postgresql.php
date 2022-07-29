7f3ad097fae2893630c9311c2d97c41bb4058074
Mundanity Fan <i@mundanity.moe>
2022-07-29 14:04:46 +0800

    更新 sql 语句的合成方式，去除条件语句中包含 ' 的情况

--- KaiheilaBot/databaseManager/postgresql.php
+++ KaiheilaBot/databaseManager/postgresql.php
@@ -13,6 +13,30 @@ class postgresql
 
     public function search($target, $table, $where1, $where2, $opt)
     {
+        //当查询的条件包含 ' 时的操作
+        $leng = strlen($where2);
+        $quote = strcspn($where2, "'");
+        if ($leng !== $quote) {
+            $array = str_split($where2);
+            $arr_len = count($array);
+            for ($i = 0; $i < $arr_len; $i++) {
+                if ($array[$i] !== "'") {
+                    continue;
+                }
+                $temp = array_chunk($array, $i + 1);
+                $temp[0][] = "'";
+                $array = array();
+                foreach ($temp as $x) {
+                    foreach ($x as $j) {
+                        $array[] = $j;
+                    }
+                }
+                $i++;
+                $arr_len++;
+            }
+            $where2 = implode($array);
+        }
+
         $sql = 'select ' . $target . ' from ' . $table . ' where ' . $where1 . ' ' . $opt . ' \'' . $where2 . '\';';
         $result = pg_query($this->dbConn, $sql);
         if (!$result) {
